6429d01d6fa410f655aafb3d6a32582e3cb5e3d1
Melody <Melody@Melodys-MacBook.local>
2017-12-18 15:57:59 -0700

    step 11

--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -22,6 +22,17 @@ const UPDATE_PROPERTY_TYPE = 'UPDATE_PTOPERTY_TYPE';
 const UPDATE_CITY = 'UPDATE_CITY';
 const UPDATE_PROP = 'UPDATE_PROP';
 const UPDATE_FOUND = 'UPDATE_FOUND';
+const UPDATE_REAL_ESTATE_AGENT = 'UPDATE_REAL_ESTATE_AGENT';
+const UPDATE_COST = 'UPDATE_COST';
+const UPDATE_DOWN_PAYMENT = 'UPDATE_DOWN_PAYMENT';
+const UPDATE_CREDIT = 'UPDATE_CREDIT';
+const UPDATE_HISTORY = 'UPDATE_HISTORY';
+const UPDATE_ADDRESS_ONE = 'UPDATE_ADDRESS_ONE';
+const UPDATE_ADDRESS_TWO = 'UPDATE_ADDRESS_TWO';
+const UPDATE_ADDRESS_THREE = 'UPDATE_ADDRESS_THREE';
+const UPDATE_FIRST_NAME = 'UPDATE_FIRST_NAME';
+const UPDATE_LAST_NAME = 'UPDATE_LAST_NAME';
+const UPDATE_EMAIL = 'UPDATE_EMAIL';
 
 function reducer(state = initialState, action){
     switch (action.type) {
@@ -35,6 +46,28 @@ function reducer(state = initialState, action){
             return {...state, propToBeUsedOn: action.payload};
         case UPDATE_FOUND:
             return {...state, found: action.payload};
+        case UPDATE_REAL_ESTATE_AGENT:
+            return {...state, realEstateAgent: action.payload};
+        case UPDATE_COST:
+            return {...state, cost: action.payload};
+        case UPDATE_DOWN_PAYMENT:
+            return {...state, downPayment: action.payload};
+        case UPDATE_CREDIT:
+            return {...state, credit: action.payload};
+        case UPDATE_HISTORY:
+            return {...state, history: action.payload};
+        case UPDATE_ADDRESS_ONE:
+            return {...state, addressOne: action.payload};
+        case UPDATE_ADDRESS_TWO:
+            return {...state, addressTwo: action.payload};
+        case UPDATE_ADDRESS_THREE:
+            return {...state, addressThree: action.payload};
+        case UPDATE_FIRST_NAME:
+            return {...state, firstName: action.payload};
+        case UPDATE_LAST_NAME:
+            return {...state, lastName: action.payload};
+        case UPDATE_EMAIL:
+            return {...state, email: action.payload};
         default:
             return state;
     }
@@ -72,6 +105,83 @@ export function updateFound (found){
     return {
         type: UPDATE_FOUND,
         payload: found
+    }
+}
+
+export function updateRealEstateAgent (realEstateAgent){
+    return {
+        type: UPDATE_REAL_ESTATE_AGENT,
+        payload: realEstateAgent
+    }
+}
+
+export function updateCost (cost){
+    return {
+        type: UPDATE_COST,
+        payload: cost
+    }
+}
+
+export function updateDownPayment (downPayment){
+    return {
+        type: UPDATE_DOWN_PAYMENT,
+        payload: downPayment
+    }
+}
+
+export function updateCredit (credit){
+    return {
+        type: UPDATE_CREDIT,
+        payload: credit
+    }
+}
+
+export function updateHistory (history){
+    return {
+        type: UPDATE_HISTORY,
+        payload: history
+    }
+}
+
+export function updateAddressOne (addressOne){
+    return {
+        type: UPDATE_ADDRESS_ONE,
+        payload: addressOne
+    }
+}
+
+export function updateAddressTwo (addressTwo){
+    return {
+        type: UPDATE_ADDRESS_TWO,
+        payload: addressTwo
+    }
+}
+
+export function updateAddressThree (addressThree){
+    return {
+        type: UPDATE_ADDRESS_THREE,
+        payload: addressThree
+    }
+}
+
+export function updateFirstName (firstName){
+    return {
+        type: UPDATE_FIRST_NAME,
+        payload: firstName
+    }
+}
+
+export function updateLastName (lastName){
+    return {
+        type: UPDATE_LAST_NAME,
+        payload: lastName
+    }
+}
+
+export function updateEmail (email){
+    return {
+        type: UPDATE_EMAIL,
+        payload: email
     }
 }
 
